fe8c430bd64205aad55ff9f9c9761d0c3b8e895c
Douglas Allen <dug_allen@yahoo.com>
2021-04-11 01:55:51 -0400

    Updates Made to Setup Creation of FHIR Prescription Bundle from Muster 16 Form Model Object.
    (1) Using CDI Event processing instead of Vert.x event bus.
    (2) Passing intermediate Muster 16 Model object to create FHIR
        precription bundle.

---
+++ src/main/java/com/erehealth/ps/model/muster16/Muster16PrescriptionForm.java
@@ -0,0 +1,155 @@
+package com.erehealth.ps.model.muster16;
+
+import java.util.List;
+
+public class Muster16PrescriptionForm {
+    private String insuranceCompany;
+    private String insuranceCompanyId;
+    private String patientFirstName;
+    private String patientLastName;
+    private String patientStreetName;
+    private String patientStreetNumber;
+    private String patientCity;
+    private String patientZipCode;
+    private String patientDateOfBirth;
+    private String clinicId;
+    private String doctorId;
+    private String prescriptionDate;
+    private List<String> prescriptionList;
+
+    public Muster16PrescriptionForm(String insuranceCompany,
+            String insuranceCompanyId,
+            String patientFirstName,
+            String patientLastName,
+            String patientStreetName,
+            String patientStreetNumber,
+            String patientCity,
+            String patientZipCode,
+            String patientDateOfBirth,
+            String clinicId,
+            String doctorId,
+            String prescriptionDate,
+            List<String> prescriptionList) {
+        this.insuranceCompany = insuranceCompany;
+        this.insuranceCompanyId = insuranceCompanyId;
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.patientStreetName = patientStreetName;
+        this.patientStreetNumber = patientStreetNumber;
+        this.patientCity = patientCity;
+        this.patientZipCode = patientZipCode;
+        this.patientDateOfBirth = patientDateOfBirth;
+        this.clinicId = clinicId;
+        this.doctorId = doctorId;
+        this.prescriptionDate = prescriptionDate;
+        this.prescriptionList = prescriptionList;
+    }
+
+    public Muster16PrescriptionForm() {
+
+    }
+
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    public void setInsuranceCompany(String insuranceCompany) {
+        this.insuranceCompany = insuranceCompany;
+    }
+
+    public String getInsuranceCompanyId() {
+        return insuranceCompanyId;
+    }
+
+    public void setInsuranceCompanyId(String insuranceCompanyId) {
+        this.insuranceCompanyId = insuranceCompanyId;
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+
+    public String getPatientStreetName() {
+        return patientStreetName;
+    }
+
+    public void setPatientStreetName(String patientStreetName) {
+        this.patientStreetName = patientStreetName;
+    }
+
+    public String getPatientStreetNumber() {
+        return patientStreetNumber;
+    }
+
+    public void setPatientStreetNumber(String patientStreetNumber) {
+        this.patientStreetNumber = patientStreetNumber;
+    }
+
+    public String getPatientCity() {
+        return patientCity;
+    }
+
+    public void setPatientCity(String patientCity) {
+        this.patientCity = patientCity;
+    }
+
+    public String getPatientZipCode() {
+        return patientZipCode;
+    }
+
+    public void setPatientZipCode(String patientZipCode) {
+        this.patientZipCode = patientZipCode;
+    }
+
+    public String getPatientDateOfBirth() {
+        return patientDateOfBirth;
+    }
+
+    public void setPatientDateOfBirth(String patientDateOfBirth) {
+        this.patientDateOfBirth = patientDateOfBirth;
+    }
+
+    public String getClinicId() {
+        return clinicId;
+    }
+
+    public void setClinicId(String clinicId) {
+        this.clinicId = clinicId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getPrescriptionDate() {
+        return prescriptionDate;
+    }
+
+    public void setPrescriptionDate(String prescriptionDate) {
+        this.prescriptionDate = prescriptionDate;
+    }
+
+    public List<String> getPrescriptionList() {
+        return prescriptionList;
+    }
+
+    public void setPrescriptionList(List<String> prescriptionList) {
+        this.prescriptionList = prescriptionList;
+    }
+}
--- src/main/java/com/erehealth/ps/service/muster16/Muster16FormDataExtractorService.java
+++ src/main/java/com/erehealth/ps/service/muster16/Muster16FormDataExtractorService.java
@@ -1,5 +1,8 @@
 package com.erehealth.ps.service.muster16;
 
+import com.erehealth.ps.model.muster16.Muster16PrescriptionForm;
+import com.erehealth.ps.service.muster16.parser.Muster16FormDataParser;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
@@ -15,5 +18,26 @@ public class Muster16FormDataExtractorService {
         String text = new PDFTextStripper().getText(doc);
 
         return text;
+    }
+
+    public Muster16PrescriptionForm extractData(String muster16PdfFileData) {
+        Muster16FormDataParser parser = new Muster16FormDataParser(muster16PdfFileData);
+        Muster16PrescriptionForm muster16Form = new Muster16PrescriptionForm(
+                parser.parseInsuranceCompany(),
+                parser.parseInsuranceCompanyId(),
+                parser.parsePatientFirstName(),
+                parser.parsePatientLastName(),
+                parser.parsePatientStreetName(),
+                parser.parsePatientStreetNumber(),
+                parser.parsePatientCity(),
+                parser.parsePatientZipCode(),
+                parser.parsePatientDateOfBirth(),
+                parser.parseClinicId(),
+                parser.parseDoctorId(),
+                parser.parsePrescriptionDate(),
+                parser.parsePrescriptionList()
+        );
+
+        return muster16Form;
     }
 }
