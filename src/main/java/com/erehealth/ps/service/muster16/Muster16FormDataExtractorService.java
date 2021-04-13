f0aefd6a4e4862c9ed44b10084e3bd47012196a3
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2021-04-13 20:49:26 +0200

    ERE-132 added personalized example for Manuel Blechschmidt (fake data) on CGM Z1 printed muster 16 form

--- src/main/java/com/erehealth/ps/service/muster16/Muster16FormDataExtractorService.java
+++ src/main/java/com/erehealth/ps/service/muster16/Muster16FormDataExtractorService.java
@@ -4,7 +4,11 @@ import com.erehealth.ps.model.muster16.Muster16PrescriptionForm;
 import com.erehealth.ps.service.muster16.parser.Muster16FormDataParser;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.util.Matrix;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,9 +17,19 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class Muster16FormDataExtractorService {
-    public String extractData(InputStream muster16PdfFile) throws IOException {
-        PDDocument doc = PDDocument.load(muster16PdfFile);
-        String text = new PDFTextStripper().getText(doc);
+    public static String extractData(InputStream muster16PdfFile) throws IOException {
+        PDDocument document = PDDocument.load(muster16PdfFile);
+        PDPage page = document.getDocumentCatalog().getPages().get(0);
+        PDPageContentStream cs = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.PREPEND, false,
+                false);
+        PDRectangle cropBox = page.getCropBox();
+        float tx = (cropBox.getLowerLeftX() + cropBox.getUpperRightX()) / 2;
+        float ty = (cropBox.getLowerLeftY() + cropBox.getUpperRightY()) / 2;
+        cs.transform(Matrix.getTranslateInstance(tx, ty));
+        cs.transform(Matrix.getRotateInstance(Math.toRadians(90), 0, 0));
+        cs.transform(Matrix.getTranslateInstance(-tx, -ty));
+        cs.close();
+        String text = new PDFTextStripper().getText(document);
 
         return text;
     }
