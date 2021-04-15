34a37ca9797d3d99e7a4b4cb2e4b9fe3856a4ec2
Douglas Allen <dug_allen@yahoo.com>
2021-04-15 03:29:14 -0400

    Created unit tests for serialization of FHIR e-prescription bundle object.
    (1) Created unit test for XML serialization.
    
    (2) Created unit test for JSON serilaization.

--- src/test/java/com/erehealth/ps/service/fhir/bundle/PrescriptionBundleBuilderTest.java
+++ src/test/java/com/erehealth/ps/service/fhir/bundle/PrescriptionBundleBuilderTest.java
@@ -1,20 +1,34 @@
 package com.erehealth.ps.service.fhir.bundle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.erehealth.ps.model.muster16.Muster16PrescriptionForm;
 
+import org.hl7.fhir.common.hapi.validation.support.CommonCodeSystemsTerminologyService;
+import org.hl7.fhir.common.hapi.validation.support.InMemoryTerminologyServerValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
+import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
 import org.hl7.fhir.r4.model.Bundle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.util.Arrays;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.SingleValidationMessage;
+import ca.uhn.fhir.validation.ValidationResult;
+
 class PrescriptionBundleBuilderTest {
-    @Test
-    public void testSuccessful_Creation_of_FHIR_EPrescription_Bundle_From_Muster16_Model_Object()
-            throws ParseException {
-        Muster16PrescriptionForm muster16PrescriptionForm = new Muster16PrescriptionForm();
+    private Muster16PrescriptionForm muster16PrescriptionForm;
+
+    @BeforeEach
+    public void initializeDefaultMuster16FormModelObject() {
+        muster16PrescriptionForm = new Muster16PrescriptionForm();
 
         muster16PrescriptionForm.setClinicId("BS12345678");
 
@@ -36,6 +50,11 @@ class PrescriptionBundleBuilderTest {
         muster16PrescriptionForm.setPatientInsuranceId("123456789");
 
         muster16PrescriptionForm.setInsuranceCompanyId("100038825");
+    }
+
+    @Test
+    public void test_Successful_Creation_of_FHIR_EPrescription_Bundle_From_Muster16_Model_Object()
+            throws ParseException {
 
         PrescriptionBundleBuilder prescriptionBundleBuilder =
                 new PrescriptionBundleBuilder(muster16PrescriptionForm);
@@ -51,6 +70,88 @@ class PrescriptionBundleBuilderTest {
         // 6. organization resource.
         // 7. coverage resource.
         assertEquals(7, fhirEPrescriptionBundle.getEntry().size());
+    }
 
+    @Test
+    public void test_Successful_XML_Serialization_Of_An_FHIR_EPrescription_Bundle_Object()
+            throws ParseException {
+        FhirContext ctx = FhirContext.forR4();
+
+        IParser parser = ctx.newXmlParser();
+
+        PrescriptionBundleBuilder prescriptionBundleBuilder =
+                new PrescriptionBundleBuilder(muster16PrescriptionForm);
+
+        Bundle fhirEPrescriptionBundle = prescriptionBundleBuilder.createBundle();
+
+        fhirEPrescriptionBundle.setId("sample-id-from-gematik-ti-123456");
+
+        parser.setPrettyPrint(true);
+
+        String serialized = parser.encodeResourceToString(fhirEPrescriptionBundle);
+
+        System.out.println(serialized);
+    }
+
+    @Test
+    public void test_Successful_JSON_Serialization_Of_An_FHIR_EPrescription_Bundle_Object()
+            throws ParseException {
+        FhirContext ctx = FhirContext.forR4();
+
+        IParser parser = ctx.newJsonParser();
+
+        PrescriptionBundleBuilder prescriptionBundleBuilder =
+                new PrescriptionBundleBuilder(muster16PrescriptionForm);
+
+        Bundle fhirEPrescriptionBundle = prescriptionBundleBuilder.createBundle();
+
+        fhirEPrescriptionBundle.setId("sample-id-from-gematik-ti-123456");
+
+        parser.setPrettyPrint(true);
+
+        String serialized = parser.encodeResourceToString(fhirEPrescriptionBundle);
+
+        System.out.println(serialized);
+    }
+
+    public void test_Successful_Validation_Of_XML_Serialization_FHIR_EPrescription_Bundle_Object()
+            throws ParseException {
+        FhirContext ctx = FhirContext.forR4();
+
+        IParser parser = ctx.newJsonParser();
+
+        PrescriptionBundleBuilder prescriptionBundleBuilder =
+                new PrescriptionBundleBuilder(muster16PrescriptionForm);
+
+        Bundle fhirEPrescriptionBundle = prescriptionBundleBuilder.createBundle();
+
+        fhirEPrescriptionBundle.setId("sample-id-from-gematik-ti-123456");
+
+        String resourceText = parser.encodeResourceToString(fhirEPrescriptionBundle);
+
+        // Create a validation support chain
+        ValidationSupportChain validationSupportChain = new ValidationSupportChain(
+                new DefaultProfileValidationSupport(ctx),
+                new InMemoryTerminologyServerValidationSupport(ctx),
+                new CommonCodeSystemsTerminologyService(ctx)
+        );
+
+        // Create a FhirInstanceValidator and register it to a validator
+        FhirValidator validator = ctx.newValidator();
+        FhirInstanceValidator instanceValidator = new FhirInstanceValidator(validationSupportChain);
+        validator.registerValidatorModule(instanceValidator);
+
+        ValidationResult validationResult = validator.validateWithResult(resourceText);
+
+        if(!validationResult.isSuccessful()) {
+            // Show the issues
+            for (SingleValidationMessage next : validationResult.getMessages()) {
+                System.out.println(" Next issue " + next.getSeverity() + " - " + next.getLocationString() + " - " + next.getMessage());
+            }
+        }
+
+        //TODO: Verify Gematik FHIR E-Prescription Bundle spec and cross reference with standard
+        // FHIR resource format requirements to fix validation errors.
+        assertTrue(validationResult.isSuccessful());
     }
 }
