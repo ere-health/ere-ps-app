5027a09c6e83c904369219d19fcdd0faace6e17e
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2021-05-08 15:36:33 +0200

    ERE-69 implemented one test case for creating a Task to get an id for an e-rezept

--- src/test/java/health/ere/ps/service/gematik/ERezeptWorkflowServiceTest.java
+++ src/test/java/health/ere/ps/service/gematik/ERezeptWorkflowServiceTest.java
@@ -1,10 +1,17 @@
 package health.ere.ps.service.gematik;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
+import org.apache.xml.security.c14n.CanonicalizationException;
+import org.apache.xml.security.c14n.InvalidCanonicalizerException;
+import org.apache.xml.security.parser.XMLParserException;
+import org.hl7.fhir.r4.model.Bundle;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import ca.uhn.fhir.context.FhirContext;
+import de.gematik.ws.conn.signatureservice.wsdl.v7.FaultMessage;
 
 public class ERezeptWorkflowServiceTest {
 
@@ -12,9 +19,14 @@ public class ERezeptWorkflowServiceTest {
 
     FhirContext fhirContext = FhirContext.forR4();
 
-    @Test
-    void testCreateERezeptOnPresciptionServer() {
-        // TODO
+    String testBearerToken = "";
+
+    @Test @Disabled
+    void testCreateERezeptOnPresciptionServer() throws InvalidCanonicalizerException, XMLParserException, CanonicalizationException, FaultMessage, IOException {
+        ERezeptWorkflowService eRezeptWorkflowService = new ERezeptWorkflowService();
+        eRezeptWorkflowService.prescriptionserverUrl = "https://fd.erezept-instanz1.titus.ti-dienste.de";
+        Bundle bundle = fhirContext.newXmlParser().parseResource(Bundle.class, getClass().getResourceAsStream("simplifier_erezept/0428d416-149e-48a4-977c-394887b3d85c.xml"));
+        eRezeptWorkflowService.createERezeptOnPresciptionServer(testBearerToken, bundle);
     }
 
     @Test
