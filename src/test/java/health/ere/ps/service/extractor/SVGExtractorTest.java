b688d2a2331796d55164cca7dae6ed82602b10e8
AndreaToth1 <andrea.toth-hp.de>
2021-05-13 11:43:24 +0200

    Do Unit testing instead of integration test

--- src/test/java/health/ere/ps/service/extractor/SVGExtractorTest.java
+++ src/test/java/health/ere/ps/service/extractor/SVGExtractorTest.java
@@ -1,46 +1,56 @@
 package health.ere.ps.service.extractor;
 
 import org.jboss.logging.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import health.ere.ps.service.muster16.Muster16FormDataExtractorService;
+import health.ere.ps.service.muster16.parser.Muster16SvgExtractorParser;
 
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.util.Map;
 
-import javax.inject.Inject;
-
-import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@QuarkusTest
 class SVGExtractorTest {
 
-    @Inject
-    SVGExtractor svgExtractor;
+    private static final String MUSTER_16_TEMPLATE_SVG = "/svg-extract-templates/Muster-16-Template.svg";
+    private static final String TESTRECIPE_PDF = "/muster-16-print-samples/cgm-z1-manuel-blechschmidt.pdf";
 
-    @Inject
-    Logger logger;
+    private SVGExtractor svgExtractor;
+
+    private Muster16FormDataExtractorService muster16FormDataExtractorService;
+
+    @Mock
+    Muster16SvgExtractorParser parser;
+
+    private Logger logger;
+
+    @BeforeEach
+    public void before() throws Exception {
+        muster16FormDataExtractorService = new Muster16FormDataExtractorService(parser);
+        this.svgExtractor = new SVGExtractor(muster16FormDataExtractorService);
+        this.svgExtractor.init(getClass().getResource(MUSTER_16_TEMPLATE_SVG).toURI(), true);
+        logger = Logger.getLogger(this.getClass());
+    }
 
     @Disabled
     @Test
     void testExtract() throws URISyntaxException {
-
-        svgExtractor.init(getClass().getResource("/svg-extract-templates/Muster-16-Template.svg").toURI(), true);
-        Map<String, String> map = svgExtractor.extract(getClass().getResourceAsStream("/muster-16-print-samples/cgm-z1-manuel-blechschmidt.pdf"));
-
-        // System.out.println(map.entrySet().stream().map((e) -> "
-        // assertEquals(\""+e.getValue().replaceAll("\n", "\\\\n")+"\",
-        // map.get(\""+e.getKey()+"\"));").collect(Collectors.joining("\n")));
-
+        
+        Map<String, String> map = svgExtractor.extract(getClass().getResourceAsStream(TESTRECIPE_PDF));
         String lineSep = System.getProperty("line.separator");
 
         assertEquals(lineSep, map.get("factor1"));
-        assertEquals("Amoxicillin 1000mg N2" + lineSep + "3x täglich alle 8 Std" + lineSep
-                + "-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -" + lineSep, map.get("medication"));
-        assertEquals("Blechschmidt" + lineSep + "Manuel               " + lineSep + "Droysenstr. 7" + lineSep + "D 10629 Berlin   "
-                + lineSep, map.get("nameAndAddress"));
+        assertEquals(
+                "Amoxicillin 1000mg N2" + lineSep + "3x täglich alle 8 Std" + lineSep + "-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -" + lineSep,
+                map.get("medication"));
+        assertEquals("Blechschmidt" + lineSep + "Manuel               " + lineSep + "Droysenstr. 7" + lineSep + "D 10629 Berlin   " + lineSep,
+                map.get("nameAndAddress"));
         assertEquals(lineSep, map.get("sprBedarf"));
         assertEquals(lineSep, map.get("factor3"));
         assertEquals(lineSep, map.get("accidentOrganization"));
@@ -53,6 +63,7 @@ class SVGExtractorTest {
 
     }
 
+  
     @Test
     void testExtract2() throws URISyntaxException, FileNotFoundException {
         svgExtractor.init(getClass().getResource("/svg-extract-templates/Muster-16-Template.svg").toURI(), true);
