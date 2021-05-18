85c185a2acb20f583361609e3449f2c6ebdd580b
AndreaToth1 <andrea.toth-hp.de>
2021-05-18 11:07:41 +0200

    Add system independent lineSeparator Add Dens receipes

--- src/test/java/health/ere/ps/service/extractor/SVGExtractorTest.java
+++ src/test/java/health/ere/ps/service/extractor/SVGExtractorTest.java
@@ -19,66 +19,68 @@ class SVGExtractorTest {
 
 
     private static Logger log = Logger.getLogger(SVGExtractorTest.class.getName());
+    
+    private String lineSep = System.lineSeparator();
 
     @Test
     void testExtract() throws URISyntaxException, IOException, XMLStreamException {
         SVGExtractor svgExtractor = new SVGExtractor(SVGExtractorConfiguration.CGM_Z1, true);
         Map<String, String> map = svgExtractor.extract(PDDocument.load(getClass().getResourceAsStream("/muster-16-print-samples/cgm-z1-manuel-blechschmidt.pdf")));
 
-        // System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll("\n", "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining("\n")));
+        //System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll(lineSep, "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining(lineSep)));
         
-        assertEquals("\n", map.get("factor1"));
-        assertEquals("Amoxicillin 1000mg N2\n3x täglich alle 8 Std\n-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -\n", map.get("medication"));
-        assertEquals("Blechschmidt\nManuel               \nDroysenstr. 7\nD 10629 Berlin   \n", map.get("nameAndAddress"));
-        assertEquals("\n", map.get("sprBedarf"));
-        assertEquals("\n", map.get("factor3"));
-        assertEquals("\n", map.get("accidentOrganization"));
-        assertEquals("\n", map.get("begrPflicht"));
-        assertEquals("30001234  \n", map.get("practitionerNumber"));
-        assertEquals(" 30001234  \n", map.get("locationNumber"));
-        assertEquals("\n", map.get("aid"));
-        assertEquals("1000000\n", map.get("status"));
-        assertEquals("\n", map.get("vaccination"));
+        assertEquals(lineSep, map.get("factor1"));
+        assertEquals("Amoxicillin 1000mg N2" + lineSep + "3x täglich alle 8 Std" + lineSep + "-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -" + lineSep, map.get("medication"));
+        assertEquals("Blechschmidt" + lineSep + "Manuel               " + lineSep + "Droysenstr. 7" + lineSep + "D 10629 Berlin   " + lineSep, map.get("nameAndAddress"));
+        assertEquals(lineSep, map.get("sprBedarf"));
+        assertEquals(lineSep, map.get("factor3"));
+        assertEquals(lineSep, map.get("accidentOrganization"));
+        assertEquals(lineSep, map.get("begrPflicht"));
+        assertEquals("30001234  " + lineSep, map.get("practitionerNumber"));
+        assertEquals(" 30001234  " + lineSep, map.get("locationNumber"));
+        assertEquals(lineSep, map.get("aid"));
+        assertEquals("1000000" + lineSep, map.get("status"));
+        assertEquals(lineSep, map.get("vaccination"));
  
     }
 
-    @Test @Disabled
-    void testExtract2() throws URISyntaxException, IOException, XMLStreamException {
+    @Test
+    void testExtractCGMTurboMed() throws URISyntaxException, IOException, XMLStreamException {
         SVGExtractor svgExtractor = new SVGExtractor(SVGExtractorConfiguration.CGM_TURBO_MED, true);
         Map<String, String> map = svgExtractor.extract(PDDocument.load(new FileInputStream("../secret-test-print-samples/CGM-Turbomed/test1.pdf")));
 
         // map.entrySet().stream().forEach(entry -> log.info(entry.getKey() +" = " + entry.getValue()));
-        // System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll("\n", "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining("\n")));
+        //  System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll(lineSep, "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining(lineSep)));
        
-        assertEquals("\n", map.get("insuranceNumber"));
-        assertEquals("\n", map.get("tax2"));
-        assertEquals("\n", map.get("bvg"));
-        assertEquals("109938331\n", map.get("payor"));
-        assertEquals("\n", map.get("prescription1"));
-        assertEquals("\n", map.get("pharmayNumber"));
-        assertEquals("\n", map.get("prescription2"));
-        assertEquals("\n", map.get("prescription3"));
-        assertEquals("\n", map.get("autIdem1"));
-        assertEquals("\n", map.get("tax3"));
-        assertEquals("\n", map.get("withPayment"));
-        assertEquals("\n", map.get("autIdem2"));
-        assertEquals("\n", map.get("accidentDate"));
-        assertEquals("\n", map.get("additionalPayment"));
-        assertEquals("\n", map.get("withoutPayment"));
-        assertEquals("\n", map.get("workAccident"));
-        assertEquals("\n", map.get("factor2"));
-        assertEquals("\n", map.get("factor1"));
-        assertEquals("Novalgin AMP N1 5X2 ml\n-  -  -  -\nPZN04527098\n", map.get("medication"));
-        assertEquals("D 56070 Koblenz\nMaria Trost 21\nDominik\nBanholzer\n", map.get("nameAndAddress"));
-        assertEquals("\n", map.get("sprBedarf"));
-        assertEquals("\n", map.get("factor3"));
-        assertEquals("\n", map.get("accidentOrganization"));
-        assertEquals("\n", map.get("begrPflicht"));
-        assertEquals("471100815\n", map.get("practitionerNumber"));
-        assertEquals("999123456\n", map.get("locationNumber"));
-        assertEquals("\n", map.get("aid"));
-        assertEquals("5000000\n", map.get("status"));
-        assertEquals("\n", map.get("vaccination"));
+        assertEquals(lineSep, map.get("insuranceNumber"));
+        assertEquals(lineSep, map.get("tax2"));
+        assertEquals(lineSep, map.get("bvg"));
+        assertEquals("109938331" + lineSep, map.get("payor"));
+        assertEquals(lineSep, map.get("prescription1"));
+        assertEquals(lineSep, map.get("pharmayNumber"));
+        assertEquals(lineSep, map.get("prescription2"));
+        assertEquals(lineSep, map.get("prescription3"));
+        assertEquals(lineSep, map.get("autIdem1"));
+        assertEquals(lineSep, map.get("tax3"));
+        assertEquals(lineSep, map.get("withPayment"));
+        assertEquals(lineSep, map.get("autIdem2"));
+        assertEquals(lineSep, map.get("accidentDate"));
+        assertEquals(lineSep, map.get("additionalPayment"));
+        assertEquals(lineSep, map.get("withoutPayment"));
+        assertEquals(lineSep, map.get("workAccident"));
+        assertEquals(lineSep, map.get("factor2"));
+        assertEquals(lineSep, map.get("factor1"));
+        assertEquals("Novalgin AMP N1 5X2 ml" + lineSep + "-  -  -  -" + lineSep + "PZN04527098" + lineSep, map.get("medication"));
+        assertEquals("D 56070 Koblenz" + lineSep +  "Maria Trost 21" + lineSep +  "Dominik" + lineSep + "Banholzer" + lineSep, map.get("nameAndAddress"));
+        assertEquals(lineSep, map.get("sprBedarf"));
+        assertEquals(lineSep, map.get("factor3"));
+        assertEquals(lineSep, map.get("accidentOrganization"));
+        assertEquals(lineSep, map.get("begrPflicht"));
+        assertEquals("471100815" + lineSep, map.get("practitionerNumber"));
+        assertEquals("999123456" + lineSep, map.get("locationNumber"));
+        assertEquals(lineSep, map.get("aid"));
+        assertEquals("5000000" + lineSep, map.get("status"));
+        assertEquals(lineSep, map.get("vaccination"));
 
 
     }
@@ -89,34 +91,126 @@ class SVGExtractorTest {
         SVGExtractor svgExtractor = new SVGExtractor(SVGExtractorConfiguration.APRAXOS, true);
         Map<String, String> map = svgExtractor.extract(PDDocument.load(getClass().getResourceAsStream("/muster-16-print-samples/apraxos_DIN_A4_Output_F-job_222.pdf")));
 
-        // System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll("\n", "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining("\n")));
         // map.entrySet().stream().forEach(entry -> log.info(entry.getKey() +" = " + entry.getValue()));
-        // System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll("\n", "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining("\n")));
-        assertEquals("AOK Bayern Die Gesundh.       \n", map.get("insurance"));
-        assertEquals("\n", map.get("autIdem3"));
-        assertEquals("          \n 25.04.21 \n", map.get("date"));
-        assertEquals("\n", map.get("noctu"));
-        assertEquals("\n", map.get("other"));
-        assertEquals("                \n                \n        16.07.86\n                \n                \n", map.get("birthdate"));
-        assertEquals("\n", map.get("pharmacyDate"));
-        assertEquals("\n", map.get("grossTotal"));
-        assertEquals("\n", map.get("accident"));
-        assertEquals("\n", map.get("tax1"));
-        assertEquals("Dr. Hans Topp-Glücklich  \nMusterstr. 1             \n18107 Rostock            \nTel 06151 1111111        \nFax 06151 2222222        \nBSNR 781234567           \nLANR 123456767           \nTopp-Gluecklich@praxis.de\n", map.get("practitionerText"));
-        assertEquals("            \n            \n", map.get("insuranceNumber"));
-        assertEquals("\n", map.get("tax2"));
-        assertEquals("\n", map.get("bvg"));
-        assertEquals("          \n108916641 \n", map.get("payor"));
+        // System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll(lineSep, "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining(lineSep)));
+        assertEquals("AOK Bayern Die Gesundh.       " + lineSep, map.get("insurance"));
+        assertEquals(lineSep, map.get("autIdem3"));
+        assertEquals("          " + lineSep + " 25.04.21 " + lineSep, map.get("date"));
+        assertEquals(lineSep, map.get("noctu"));
+        assertEquals(lineSep, map.get("other"));
+        assertEquals("                " + lineSep + "                " + lineSep + "        16.07.86" + lineSep + "                " + lineSep + "                " + lineSep, map.get("birthdate"));
+        assertEquals(lineSep, map.get("pharmacyDate"));
+        assertEquals(lineSep, map.get("grossTotal"));
+        assertEquals(lineSep, map.get("accident"));
+        assertEquals(lineSep, map.get("tax1"));
+        assertEquals("Dr. Hans Topp-Glücklich  " + lineSep + "Musterstr. 1             " + lineSep + "18107 Rostock            " + lineSep + "Tel 06151 1111111        " + lineSep + "Fax 06151 2222222        " + lineSep + "BSNR 781234567           " + lineSep +  "LANR 123456767           " + lineSep +  "Topp-Gluecklich@praxis.de" + lineSep, map.get("practitionerText"));
+        assertEquals("            " + lineSep + "            " + lineSep, map.get("insuranceNumber"));
+        assertEquals(lineSep, map.get("tax2"));
+        assertEquals(lineSep, map.get("bvg"));
+        assertEquals("          " + lineSep + "108916641 " + lineSep, map.get("payor"));
 
     }
 
     @Test @Disabled
-    void testExtractDens() throws URISyntaxException, IOException, XMLStreamException {
+    void testExtractDens1() throws URISyntaxException, IOException, XMLStreamException {
         SVGExtractor svgExtractor = new SVGExtractor(SVGExtractorConfiguration.DENS, true);
         Map<String, String> map = svgExtractor.extract(PDDocument.load(new FileInputStream("../secret-test-print-samples/DENS-GmbH/DENSoffice - Rezept1.pdf")));
 
         // map.entrySet().stream().forEach(entry -> log.info(entry.getKey() +" = " + entry.getValue()));
-        System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll("\n", "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining("\n")));
+        // System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll(lineSep, "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining(lineSep)));
+        
+        assertEquals("DENS GmbH                     " + lineSep, map.get("insurance"));
+        assertEquals(lineSep, map.get("autIdem3"));
+        assertEquals("  29.04.21" + lineSep, map.get("date"));
+        assertEquals(lineSep, map.get("noctu"));
+        assertEquals(lineSep, map.get("other"));
+        assertEquals("        14.02.76" + lineSep + "12" + lineSep + "           " + lineSep, map.get("birthdate"));
+        assertEquals(lineSep, map.get("pharmacyDate"));
+        assertEquals(lineSep, map.get("grossTotal"));
+        assertEquals(lineSep, map.get("accident"));
+        assertEquals(lineSep, map.get("tax1"));
+        assertEquals("DENS GmbH" + lineSep + "Berliner Str. 13" + lineSep + "14513 Teltow" + lineSep + "03328-334540" + lineSep + "Fax: 03328-334547" + lineSep, map.get("practitionerText"));
+        assertEquals("           " + lineSep, map.get("insuranceNumber"));
+        assertEquals(lineSep, map.get("tax2"));
+        assertEquals(lineSep, map.get("bvg"));
+        assertEquals("          " + lineSep, map.get("payor"));
+        assertEquals(lineSep, map.get("prescription1"));
+        assertEquals(lineSep, map.get("pharmayNumber"));
+        assertEquals(lineSep, map.get("prescription2"));
+        assertEquals(lineSep, map.get("prescription3"));
+        assertEquals(lineSep, map.get("autIdem1"));
+        assertEquals(lineSep, map.get("tax3"));
+        assertEquals("X" + lineSep, map.get("withPayment"));
+        assertEquals(lineSep, map.get("autIdem2"));
+        assertEquals(lineSep, map.get("accidentDate"));
+        assertEquals(lineSep, map.get("additionalPayment"));
+        assertEquals(lineSep, map.get("withoutPayment"));
+        assertEquals(lineSep, map.get("workAccident"));
+        assertEquals(lineSep, map.get("factor2"));
+        assertEquals(lineSep, map.get("factor1"));
+        assertEquals("Ibuprofen 600mg 1-1-1" + lineSep + "Omeprazol  40 mg  0-0-1" + lineSep + "Amoxicillin 1.000 mg 1-0-1" + lineSep, map.get("medication"));
+        assertEquals("Heckner" + lineSep + "Dr. Markus    " + lineSep + "Berliner Str. " + lineSep + "D 14513 Teltow" + lineSep, map.get("nameAndAddress"));
+        assertEquals(lineSep, map.get("sprBedarf"));
+        assertEquals(lineSep, map.get("factor3"));
+        assertEquals(lineSep, map.get("accidentOrganization"));
+        assertEquals(lineSep, map.get("begrPflicht"));
+        assertEquals("  30000000" + lineSep, map.get("practitionerNumber"));
+        assertEquals(" 30000000" + lineSep, map.get("locationNumber"));
+        assertEquals(lineSep, map.get("aid"));
+        assertEquals("  3000000" + lineSep, map.get("status"));
+        assertEquals(lineSep, map.get("vaccination"));
+
+    }
+    
+    @Test @Disabled
+    void testExtractDens2() throws URISyntaxException, IOException, XMLStreamException {
+        SVGExtractor svgExtractor = new SVGExtractor(SVGExtractorConfiguration.DENS, true);
+        Map<String, String> map = svgExtractor.extract(PDDocument.load(new FileInputStream("../secret-test-print-samples/DENS-GmbH/DENSoffice - Rezept2.pdf")));
+
+        // map.entrySet().stream().forEach(entry -> log.info(entry.getKey() +" = " + entry.getValue()));
+        System.out.println(map.entrySet().stream().map((e) -> "        assertEquals(\""+e.getValue().replaceAll(lineSep, "\\\\n")+"\", map.get(\""+e.getKey()+"\"));").collect(Collectors.joining(lineSep)));
+        
+        assertEquals("DENS GmbH                     " + lineSep, map.get("insurance"));
+        assertEquals(lineSep, map.get("autIdem3"));
+        assertEquals("  29.04.21" + lineSep, map.get("date"));
+        assertEquals(lineSep, map.get("noctu"));
+        assertEquals(lineSep, map.get("other"));
+        assertEquals("        14.02.76" + lineSep + "12" + lineSep + "           " + lineSep, map.get("birthdate"));
+        assertEquals("age" + lineSep, map.get("pharmacyDate"));
+        assertEquals(lineSep, map.get("grossTotal"));
+        assertEquals(lineSep, map.get("accident"));
+        assertEquals(lineSep, map.get("tax1"));
+        assertEquals("DENS GmbH" + lineSep + "Berliner Str. 13" + lineSep + "14513 Teltow" + lineSep + "03328-334540" + lineSep + "Fax: 03328-334547" + lineSep, map.get("practitionerText"));
+        assertEquals("           " + lineSep, map.get("insuranceNumber"));
+        assertEquals(lineSep, map.get("tax2"));
+        assertEquals(lineSep, map.get("bvg"));
+        assertEquals("          " + lineSep, map.get("payor"));
+        assertEquals(lineSep, map.get("prescription1"));
+        assertEquals(lineSep, map.get("pharmayNumber"));
+        assertEquals(lineSep, map.get("prescription2"));
+        assertEquals(lineSep, map.get("prescription3"));
+        assertEquals(lineSep, map.get("autIdem1"));
+        assertEquals(lineSep, map.get("tax3"));
+        assertEquals("X" + lineSep, map.get("withPayment"));
+        assertEquals(lineSep, map.get("autIdem2"));
+        assertEquals(lineSep, map.get("accidentDate"));
+        assertEquals(lineSep, map.get("additionalPayment"));
+        assertEquals(lineSep, map.get("withoutPayment"));
+        assertEquals(lineSep, map.get("workAccident"));
+        assertEquals(lineSep, map.get("factor2"));
+        assertEquals(lineSep, map.get("factor1"));
+        assertEquals("Metamizol 20 Topfen/500mg bei" + lineSep + "Bedarf, Tageshöchstdosis: 1.5Pantoprazol 40mg 1-0-0" + lineSep + "Clindamycin 600mg 1-0-1 für 5" + lineSep, map.get("medication"));
+        assertEquals("Heckner" + lineSep + "Dr. Markus    " + lineSep + "Berliner Str. " + lineSep + "D 14513 Teltow" + lineSep, map.get("nameAndAddress"));
+        assertEquals(lineSep, map.get("sprBedarf"));
+        assertEquals(lineSep, map.get("factor3"));
+        assertEquals(lineSep, map.get("accidentOrganization"));
+        assertEquals(lineSep, map.get("begrPflicht"));
+        assertEquals("  30000000" + lineSep, map.get("practitionerNumber"));
+        assertEquals(" 30000000" + lineSep, map.get("locationNumber"));
+        assertEquals(lineSep, map.get("aid"));
+        assertEquals("  3000000" + lineSep, map.get("status"));
+        assertEquals(lineSep, map.get("vaccination"));
+
        
     }
 
