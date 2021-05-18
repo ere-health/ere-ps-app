1e23e5b2a252f9a9011dcae3b375bf29aa942761
OmarMalass <Omar1Malass@gmail.com>
2021-05-18 19:14:21 +0300

    Moved Print-Operations handling to IppPrinter. - Encapsulated all printer-related fields within IppPrinter.

--- src/main/java/health/ere/ps/resource/PrinterResource.java
+++ src/main/java/health/ere/ps/resource/PrinterResource.java
@@ -1,13 +1,8 @@
 package health.ere.ps.resource;
 
-import static com.hp.jipp.encoding.AttributeGroup.groupOf;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URI;
-import java.util.Collections;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Event;
@@ -22,19 +17,10 @@ import javax.ws.rs.core.UriInfo;
 
 import com.hp.jipp.encoding.IppInputStream;
 import com.hp.jipp.encoding.IppOutputStream;
-import com.hp.jipp.encoding.IppPacket;
-import com.hp.jipp.encoding.Tag;
-import com.hp.jipp.model.JobState;
-import com.hp.jipp.model.JobStateReason;
-import com.hp.jipp.model.Operation;
-import com.hp.jipp.model.Status;
-import com.hp.jipp.model.Types;
 import com.hp.jipp.trans.IppPacketData;
 import com.hp.jipp.trans.IppServerTransport;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-
 import health.ere.ps.event.PDDocumentEvent;
+
 
 @Path("ipp")
 public class PrinterResource implements IppServerTransport {
@@ -42,10 +28,14 @@ public class PrinterResource implements IppServerTransport {
     @Inject
     Event<PDDocumentEvent> pdDocumentEvent;
 
-    private final IppPrinter printer = new IppPrinter();
-    private final String IPP_MEDIA_TYPE = "application/ipp";
+    @Inject
+    public PrinterResource(Event<PDDocumentEvent> pdDocumentEvent) {
+        this.pdDocumentEvent = pdDocumentEvent;
+        printer = new IppPrinter(this.pdDocumentEvent);
+    }
 
-    private AtomicInteger printJobId = new AtomicInteger(0);
+    private final IppPrinter printer;
+    private final String IPP_MEDIA_TYPE = "application/ipp";
 
     private static Logger log = Logger.getLogger(PrinterResource.class.getName());
 
@@ -69,37 +59,8 @@ public class PrinterResource implements IppServerTransport {
     public IppPacketData handle(URI uri, IppPacketData data) throws IOException {
         log.info(uri+" was called ");
         log.info("Request: "+data);
-        IppPacket ippPacket = data.getPacket();
-        if(ippPacket.getOperation().equals(Operation.getPrinterAttributes)) {
-            IppPacketData serverResponse = printer.handleGetPrinterAttributesOperation(uri, data);
-            log.info("Response: "+serverResponse);
-            return serverResponse;
-        }
-
-        if(ippPacket.getOperation().equals(Operation.getJobs)) {
-            IppPacket responsePacket = new IppPacket(Status.successfulOk, ippPacket.getRequestId(),
-            groupOf(Tag.operationAttributes),
-            groupOf(Tag.printerAttributes));
-            IppPacketData serverResponse = new IppPacketData(responsePacket, null);
-            log.info("Response: "+serverResponse);
-            return serverResponse;
-        }
-
-        if(ippPacket.getOperation().equals(Operation.printJob)) {
-            // TODO: check for mime type, for the moment, expect PDF
-            pdDocumentEvent.fireAsync(new PDDocumentEvent(PDDocument.load(data.getData())));
-            IppPacket responsePacket = IppPacket.jobResponse(
-                Status.successfulOk, ippPacket.getRequestId(), uri.resolve("/job/"+printJobId.incrementAndGet()),
-                JobState.pending,
-                Collections.singletonList(JobStateReason.accountClosed))
-                .putAttributes(Tag.operationAttributes, Types.printerUri.of(uri))
-            .build();
-            IppPacketData serverResponse = new IppPacketData(responsePacket, null);
-            log.info("Response: "+serverResponse);
-            return serverResponse;
-        }
-
-        return data;
+        IppPacketData serverResponse = printer.handleIppPacketData(uri, data);
+        log.info("Response: "+serverResponse);
+        return serverResponse;
     }
-
 }
