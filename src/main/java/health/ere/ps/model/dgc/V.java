d2fb4bf2887daa91348223cfc2f16a1d89e45093
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2021-06-03 12:25:03 +0200

    Added all components for COVID-19 certificate solution, TODO: connect frontend to backend

---
+++ src/main/java/health/ere/ps/model/dgc/V.java
@@ -0,0 +1,14 @@
+package health.ere.ps.model.dgc;
+
+public class V{
+    public String tg;
+    public String vp;
+    public String mp;
+    public String ma;
+    public int dn;
+    public int sd;
+    public String dt;
+    public String co;
+    public String is;
+    public String ci;
+}
