f7cdcdf965e1c8914b07030212fcdbc9e479b04b
Tempuser 5432 <temp_user_5432@infai.org>
2021-06-07 15:45:32 +0200

    Add equals and hashCode methods to dtos
    The methods are needed for assertions that will be used during
    tests.
    The methods have been generated by intellij.

--- src/main/java/health/ere/ps/model/dgc/Nam.java
+++ src/main/java/health/ere/ps/model/dgc/Nam.java
@@ -1,6 +1,21 @@
 package health.ere.ps.model.dgc;
 
+import java.util.Objects;
+
 public class Nam {
     public String fn;
     public String gn;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nam nam = (Nam) o;
+        return Objects.equals(fn, nam.fn) && Objects.equals(gn, nam.gn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fn, gn);
+    }
 }
