4e8e2693bc623509c433dac859fb14f6e1c68611
Tempuser 5432 <temp_user_5432@infai.org>
2021-06-07 17:11:06 +0200

    Add method to issue certificate using flat parameters

--- src/main/java/health/ere/ps/service/dgc/DigitalGreenCertificateService.java
+++ src/main/java/health/ere/ps/service/dgc/DigitalGreenCertificateService.java
@@ -1,6 +1,8 @@
 package health.ere.ps.service.dgc;
 
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -13,6 +15,9 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
+import com.google.common.base.Strings;
+import health.ere.ps.model.dgc.Nam;
+import health.ere.ps.model.dgc.V;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import health.ere.ps.event.RequestBearerTokenFromIdpEvent;
@@ -35,7 +40,7 @@ public class DigitalGreenCertificateService {
         ClientBuilder clientBuilder = ClientBuilder.newBuilder();
         client = clientBuilder.build();
     }
-    
+
     public byte[] issue(VaccinationCertificateRequest vaccinationCertificateRequest) {
         Response response = client.target(issuerAPIUrl)
                 .request("application/pdf")
@@ -55,11 +60,78 @@ public class DigitalGreenCertificateService {
         return pdf;
     }
 
+    /**
+     * Issue a certificate based on the given values.
+     *
+     * @param fn  full name
+     * @param gn  given name
+     * @param dob date of birth
+     * @param id1 administering instance id 1
+     * @param tg1 illness 1
+     * @param vp1 vaccine 1
+     * @param mp1 product 1
+     * @param ma1 manufacturer 1
+     * @param dn1 dose number 1
+     * @param sd1 total dose count 1
+     * @param dt1 vaccination date 1
+     * @param id2 administering instance id 2; the second vaccination will be added to the certificate iff this value is non-empty
+     * @param tg2 illness 2
+     * @param vp2 vaccine 2
+     * @param mp2 product 2
+     * @param ma2 manufacturer 2
+     * @param dn2 dose number 2
+     * @param sd2 total dose count 2
+     * @param dt2 vaccination date 2
+     * @return bytes of certificate pdf
+     */
+    public byte[] issue(String fn, String gn, String dob,
+                        String id1, String tg1, String vp1, String mp1, String ma1, Integer dn1, Integer sd1,
+                        String dt1,
+                        String id2, String tg2, String vp2, String mp2, String ma2, Integer dn2, Integer sd2,
+                        String dt2) {
+
+        VaccinationCertificateRequest vaccinationCertificateRequest = new VaccinationCertificateRequest();
+
+        vaccinationCertificateRequest.dob = dob;
+
+        Nam nam = new Nam();
+
+        nam.gn = gn;
+        nam.fn = fn;
+
+        vaccinationCertificateRequest.nam = nam;
+
+        V v1 = createV(id1, tg1, vp1, mp1, ma1, dn1, sd1, dt1);
+
+        if (Strings.isNullOrEmpty(id2)) {
+            vaccinationCertificateRequest.v = Collections.singletonList(v1);
+        } else {
+            vaccinationCertificateRequest.v = List.of(v1, createV(id2, tg2, vp2, mp2, ma2, dn2, sd2, dt2));
+        }
+
+        return issue(vaccinationCertificateRequest);
+    }
+
     private String getToken() {
         RequestBearerTokenFromIdpEvent event = new RequestBearerTokenFromIdpEvent();
 
         requestBearerTokenFromIdp.fire(event);
 
         return Optional.ofNullable(event.getBearerToken()).orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static V createV(String id, String tg, String vp, String mp, String ma, Integer dn, Integer sd, String dt) {
+        V v = new V();
+
+        v.id = id;
+        v.tg = tg;
+        v.vp = vp;
+        v.mp = mp;
+        v.ma = ma;
+        v.dn = dn;
+        v.sd = sd;
+        v.dt = dt;
+
+        return v;
     }
 }
