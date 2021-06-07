f998bc37007ef0a514203e5c7a4f9d88d06154b9
Tempuser 5432 <temp_user_5432@infai.org>
2021-06-07 14:10:33 +0200

    Fix dcg dtos

--- src/main/java/health/ere/ps/model/dgc/V.java
+++ src/main/java/health/ere/ps/model/dgc/V.java
@@ -1,6 +1,6 @@
 package health.ere.ps.model.dgc;
 
-public class V{
+public class V {
     public String id;
     public String tg;
     public String vp;
@@ -9,7 +9,4 @@ public class V{
     public Integer dn;
     public Integer sd;
     public String dt;
-    public String co;
-    public String is;
-    public String ci;
 }
