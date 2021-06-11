b47863ba1ea68949e7ef0fca29994acf862aa786
Tempuser 5432 <temp_user_5432@infai.org>
2021-06-11 08:26:32 +0200

    Add test for different response code
    The code has been copied from the edited file!

--- src/test/java/health/ere/ps/service/dgc/DigitalGreenCertificateServiceTest.java
+++ src/test/java/health/ere/ps/service/dgc/DigitalGreenCertificateServiceTest.java
@@ -1,6 +1,10 @@
 package health.ere.ps.service.dgc;
 
 import health.ere.ps.event.RequestBearerTokenFromIdpEvent;
+import health.ere.ps.exception.dgc.DgcCertificateServiceAuthenticationException;
+import health.ere.ps.exception.dgc.DgcCertificateServiceException;
+import health.ere.ps.exception.dgc.DgcException;
+import health.ere.ps.exception.dgc.DgcInvalidParametersException;
 import health.ere.ps.model.dgc.CertificateRequest;
 import health.ere.ps.model.dgc.PersonName;
 import health.ere.ps.model.dgc.V;
@@ -22,6 +26,7 @@ import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
@@ -35,45 +40,15 @@ class DigitalGreenCertificateServiceTest {
     private DigitalGreenCertificateService digitalGreenCertificateService;
 
     @Test
-    void issue() throws IOException {
-        // mock token
-        String token = "testToken";
-
-        @SuppressWarnings("unchecked")
-        Event<RequestBearerTokenFromIdpEvent> requestBearerTokenFromIdpEventEvent = mock(Event.class);
-
-        doAnswer((invocation) -> {
-            ((RequestBearerTokenFromIdpEvent) invocation.getArgument(0)).setBearerToken(token);
-            return null;
-        }).when(requestBearerTokenFromIdpEventEvent).fire(any());
-
-        digitalGreenCertificateService.requestBearerTokenFromIdp = requestBearerTokenFromIdpEventEvent;
-
-        // mock web request
-        Client client = mock(Client.class);
-
-        String issuerAPIUrl = "testIssuerAPIUrl";
-
-        WebTarget webTarget = mock(WebTarget.class);
-
-        Invocation.Builder builder1 = mock(Invocation.Builder.class);
-
-        Invocation.Builder builder2 = mock(Invocation.Builder.class);
+    void issuePdf() throws IOException {
+        InputStream inputStream = mock(InputStream.class);
 
         CertificateRequest certificateRequest = mock(CertificateRequest.class);
 
-        Response response = mock(Response.class);
-
-        InputStream inputStream = mock(InputStream.class);
-
         byte[] bytes = new byte[]{34, 45, 56};
 
-        digitalGreenCertificateService.issuerAPIUrl = issuerAPIUrl;
-        digitalGreenCertificateService.client = client;
-        when(client.target(issuerAPIUrl)).thenReturn(webTarget);
-        when(webTarget.request("application/pdf")).thenReturn(builder1);
-        when(builder1.header("Authorization", "Bearer " + token)).thenReturn(builder2);
-        when(builder2.post(Entity.entity(certificateRequest, "application/vnd.dgc.v1+json"))).thenReturn(response);
+        Response response = mockAuthenticatedResponse(certificateRequest);
+
         when(response.getStatus()).thenReturn(200);
         when(response.readEntity(InputStream.class)).thenReturn(inputStream);
         when(inputStream.readAllBytes()).thenReturn(bytes);
@@ -82,6 +57,18 @@ class DigitalGreenCertificateServiceTest {
 
         verifyNoInteractions(certificateRequest);
     }
+
+    @Test
+    void issuePdfWithCertificateServiceException() {
+        issuePdfWithCertificateServiceException(401, DgcCertificateServiceAuthenticationException.class, 100401);
+        issuePdfWithCertificateServiceException(403, DgcCertificateServiceAuthenticationException.class, 100403);
+        issuePdfWithCertificateServiceException(400, DgcInvalidParametersException.class, 100400);
+        issuePdfWithCertificateServiceException(406, DgcInvalidParametersException.class, 100406);
+        issuePdfWithCertificateServiceException(500, DgcCertificateServiceException.class, 100500);
+        // other unknown codes
+        issuePdfWithCertificateServiceException(543, DgcCertificateServiceException.class, 100543);
+    }
+
 
     @Test
     void issueVaccinationCertificate() {
@@ -136,5 +123,57 @@ class DigitalGreenCertificateServiceTest {
 
         assertEquals(response, digitalGreenCertificateService.issueVaccinationCertificatePdf(fn, gn, dob, id, tg, vp, mp,
                 ma, dn, sd, dt));
+    }
+
+    private void issuePdfWithCertificateServiceException(int responseCode, Class<? extends DgcException> expectedExceptionClass,
+                                                         int expectedErrorCode) {
+
+        CertificateRequest certificateRequest = mock(CertificateRequest.class);
+
+        Response response = mockAuthenticatedResponse(certificateRequest);
+
+        when(response.getStatus()).thenReturn(responseCode);
+
+        DgcException dgcException = assertThrows(expectedExceptionClass
+                ,() -> digitalGreenCertificateService.issuePdf(certificateRequest));
+
+        assertEquals(expectedErrorCode, dgcException.getCode());
+    }
+
+    private Response mockAuthenticatedResponse(CertificateRequest certificateRequest) {
+        // mock token
+        String token = "testToken";
+
+        @SuppressWarnings("unchecked")
+        Event<RequestBearerTokenFromIdpEvent> requestBearerTokenFromIdpEventEvent = mock(Event.class);
+
+        doAnswer((invocation) -> {
+            ((RequestBearerTokenFromIdpEvent) invocation.getArgument(0)).setBearerToken(token);
+            return null;
+        }).when(requestBearerTokenFromIdpEventEvent).fire(any());
+
+        digitalGreenCertificateService.requestBearerTokenFromIdp = requestBearerTokenFromIdpEventEvent;
+
+        // mock web request
+        Client client = mock(Client.class);
+
+        String issuerAPIUrl = "testIssuerAPIUrl";
+
+        WebTarget webTarget = mock(WebTarget.class);
+
+        Invocation.Builder builder1 = mock(Invocation.Builder.class);
+
+        Invocation.Builder builder2 = mock(Invocation.Builder.class);
+
+        Response response = mock(Response.class);
+
+        digitalGreenCertificateService.issuerAPIUrl = issuerAPIUrl;
+        digitalGreenCertificateService.client = client;
+        when(client.target(issuerAPIUrl)).thenReturn(webTarget);
+        when(webTarget.request("application/pdf")).thenReturn(builder1);
+        when(builder1.header("Authorization", "Bearer " + token)).thenReturn(builder2);
+        when(builder2.post(Entity.entity(certificateRequest, "application/vnd.dgc.v1+json"))).thenReturn(response);
+
+        return response;
     }
 }
