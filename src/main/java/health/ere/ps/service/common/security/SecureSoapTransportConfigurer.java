3f6455ffa0b7600d473c605ff82a920062f72efd
Douglas Allen <dug_allen@yahoo.com>
2021-06-10 22:20:01 -0400

    ERE-363 Implementing test cases for retrieval of card handles from connector

---
+++ src/main/java/health/ere/ps/service/common/security/SecureSoapTransportConfigurer.java
@@ -0,0 +1,37 @@
+package health.ere.ps.service.common.security;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.xml.ws.BindingProvider;
+
+import health.ere.ps.exception.common.security.SecretsManagerException;
+
+@Dependent
+public class SecureSoapTransportConfigurer {
+    @Inject
+    SecretsManagerService secretsManagerService;
+
+    private BindingProvider bindingProvider;
+
+    public void init(SoapClient soapClient) {
+        this.bindingProvider = bindingProvider;
+    }
+
+    public void configureSecureTransport(String endpointAddress,
+                                         SecretsManagerService.SslContextType sslContextType,
+                                         String tlsCertTrustStore,
+                                         String tlsCertTrustStorePassword) throws SecretsManagerException {
+        if (bindingProvider != null && StringUtils.isNotBlank(endpointAddress) &&
+                StringUtils.isNotBlank(tlsCertTrustStore) &&
+                StringUtils.isNotBlank(tlsCertTrustStorePassword)) {
+            bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                    endpointAddress);
+
+            secretsManagerService.configureSSLTransportContext(tlsCertTrustStore,
+                    tlsCertTrustStorePassword, sslContextType,
+                    SecretsManagerService.KeyStoreType.PKCS12, bindingProvider);
+        }
+    }
+}
