2afe8b634c1a317de4d07f64c68c4fe280631d17
Douglas Allen <dug_allen@yahoo.com>
2021-06-30 21:07:01 -0400

    Added fix for the FHIR Bundle-to-Json Encoder Dropping Json Elements
    (1) Created a parsing strategy to avoid the use of the FHIR parser that
        encodes a Java bundle object to a Json String. The current bundle to
        json serialisation method seems to silently alter the structure of the
        generated Json string by removing elements that have data in formats
        that it either cannot convert to or from a related Java data type or
        which violates its schema.
    
        Based on the nature of this problem, no guarantees can be made about the
        anticipated structure of the generated Json string when using the FHIR
        encoder to serialise the Java bundle object.
    
    (2) Created EreBundle class.
    
    (3) Modified event intercepting components along the unaltered event chain that is used
        to produce and transmit the initial bundle to the FE. The chain now transmits EreBundle objects
        as an event object payload.
    
    (4) An EreBundle has been implemented to serlialise itself to Json and to
        maintain a compatible and consistent structure with its source template.
    
    (5) Updated test cases.
    
    (6) General code cleanup and refinements.

--- src/main/java/health/ere/ps/service/fhir/FHIRService.java
+++ src/main/java/health/ere/ps/service/fhir/FHIRService.java
@@ -2,7 +2,6 @@ package health.ere.ps.service.fhir;
 
 import org.hl7.fhir.r4.model.Bundle;
 
-import java.text.ParseException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,12 +11,12 @@ import javax.enterprise.event.Event;
 import javax.enterprise.event.ObservesAsync;
 import javax.inject.Inject;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
+import health.ere.ps.config.AppConfig;
 import health.ere.ps.event.BundlesEvent;
 import health.ere.ps.event.Muster16PrescriptionFormEvent;
+import health.ere.ps.exception.bundle.EreParseException;
 import health.ere.ps.model.muster16.Muster16PrescriptionForm;
-import health.ere.ps.service.fhir.bundle.IBundlesBuilder;
+import health.ere.ps.service.fhir.bundle.EreBundle;
 import health.ere.ps.service.fhir.bundle.PrescriptionBundlesBuilderV2;
 import health.ere.ps.validation.fhir.bundle.PrescriptionBundleValidator;
 
@@ -25,6 +24,9 @@ import health.ere.ps.validation.fhir.bundle.PrescriptionBundleValidator;
 public class FHIRService {
 
     private static final Logger log = Logger.getLogger(FHIRService.class.getName());
+
+    @Inject
+    AppConfig appConfig;
 
     @Inject
     PrescriptionBundleValidator prescriptionBundleValidator;
@@ -37,36 +39,27 @@ public class FHIRService {
 
     public void generatePrescriptionBundle(@ObservesAsync Muster16PrescriptionFormEvent muster16PrescriptionFormEvent) {
         Muster16PrescriptionForm muster16PrescriptionForm = muster16PrescriptionFormEvent.getMuster16PrescriptionForm();
-        IBundlesBuilder bundleBuilder = new PrescriptionBundlesBuilderV2(muster16PrescriptionForm);
+        PrescriptionBundlesBuilderV2 bundleBuilder = new PrescriptionBundlesBuilderV2(muster16PrescriptionForm);
 
-        try {
-            List<Bundle> bundles = bundleBuilder.createBundles();
-            FhirContext ctx = FhirContext.forR4();
+        List<Bundle> bundles = bundleBuilder.createBundles();
 
-            IParser jsonParser = ctx.newJsonParser();
+        bundles.forEach(bundle -> {
 
-            jsonParser.setPrettyPrint(true);
+            // Warning: Do not use return value of validation method below to stop submission of
+            // possible problem bundle to front-end. For now, only report possible
+            // validation issues for generated outgoing bundle for review in the logs.
 
-            bundles.forEach(bundle -> {
-                String jsonSerialized = jsonParser.encodeResourceToString(bundle);
+            //TODO: Create a separate configuration to enable/disable validating outgoing
+            // bundles.
+            if (appConfig.isValidateSignRequestBundles())
+                isOutgoingBundleOk((EreBundle) bundle);
 
-                log.info(String.format("Bundle builder created bundle: %s with validation results" +
-                        " shown below:", jsonSerialized));
-
-                // Warning: Do not use return value of validation method below to stop submission of
-                // possible problem bundle to front-end. For now, only report possible
-                // validation issues for generated outgoing bundle for review in the logs.
-                isOutgoingBundleOk(bundle);
-                bundleEvent.fireAsync(new BundlesEvent(bundle));
-            });
-        } catch (ParseException e) {
-            log.log(Level.SEVERE, "Exception encountered while generating e-prescription bundle" +
-                    ".", e);
-            exceptionEvent.fireAsync(e);
-        }
+            bundleEvent.fireAsync(new BundlesEvent(bundle));
+        });
     }
 
-    private boolean isOutgoingBundleOk(Bundle bundle) {
-        return prescriptionBundleValidator.validateResource(bundle, true).isSuccessful();
+    private boolean isOutgoingBundleOk(EreBundle bundle) {
+        return prescriptionBundleValidator.validateResource(bundle.encodeToJson(),
+                true).isSuccessful();
     }
 }
