3cc6546b0e11f58c50904c2e48b0098847e242e4
Douglas Allen <dug_allen@yahoo.com>
2021-07-11 15:57:26 -0400

    Minor code cleanup

--- src/test/java/health/ere/ps/service/logging/EreLoggerTest.java
+++ src/test/java/health/ere/ps/service/logging/EreLoggerTest.java
@@ -1,7 +1,5 @@
 package health.ere.ps.service.logging;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
