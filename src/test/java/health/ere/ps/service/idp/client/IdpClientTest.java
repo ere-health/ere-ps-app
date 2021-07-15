2814dfffd681e1fbf89958b37af015b5a34321be
barelyThinkingBagOfWater <x.barrelet@gmail.com>
2021-07-15 07:42:01 +0700

    Removed useless part of a test

--- src/test/java/health/ere/ps/service/idp/client/IdpClientTest.java
+++ src/test/java/health/ere/ps/service/idp/client/IdpClientTest.java
@@ -1,19 +1,5 @@
 package health.ere.ps.service.idp.client;
 
-import java.io.IOException;
-import java.security.Security;
-import java.security.cert.X509Certificate;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import health.ere.ps.config.AppConfig;
 import health.ere.ps.exception.connector.ConnectorCardCertificateReadException;
 import health.ere.ps.exception.connector.ConnectorCardsException;
@@ -21,20 +7,22 @@ import health.ere.ps.exception.idp.IdpClientException;
 import health.ere.ps.exception.idp.IdpException;
 import health.ere.ps.exception.idp.IdpJoseException;
 import health.ere.ps.model.idp.client.IdpTokenResult;
+import health.ere.ps.profile.RUTestProfile;
 import health.ere.ps.profile.TitusTestProfile;
 import health.ere.ps.service.connector.cards.ConnectorCardsService;
 import health.ere.ps.service.connector.certificate.CardCertificateReaderService;
-import health.ere.ps.service.connector.endpoint.SSLUtilities;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.security.cert.X509Certificate;
+import java.util.logging.Logger;
 
 @QuarkusTest
-@TestProfile(TitusTestProfile.class)
+@TestProfile(RUTestProfile.class)
 public class IdpClientTest {
-
-    static {
-        Security.addProvider(new BouncyCastleProvider());
-    }
 
     private final Logger log = Logger.getLogger(getClass().getName());
 
@@ -49,33 +37,6 @@ public class IdpClientTest {
 
     String discoveryDocumentUrl;
 
-    @BeforeAll
-    public static void init() {
-
-        SSLUtilities.trustAllHostnames();
-        SSLUtilities.trustAllHttpsCertificates();
-
-        try {
-            // https://community.oracle.com/thread/1307033?start=0&tstart=0
-            LogManager.getLogManager().readConfiguration(
-                    IdpClientTest.class
-                            .getResourceAsStream("/logging.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
-        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
-        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
-    }
-
-    @BeforeEach
-    void configureSecureTransport() {
-        SSLUtilities.trustAllHostnames();
-        SSLUtilities.trustAllHttpsCertificates();
-    }
 
     @Test
     public void test_Successful_Idp_Login_With_Connector_Smcb() throws IdpJoseException,
