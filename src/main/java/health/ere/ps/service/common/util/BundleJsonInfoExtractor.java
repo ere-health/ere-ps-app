2e35a8e89f182539e99544895b3eeb657243f63c
Douglas Allen <dug_allen@yahoo.com>
2021-07-16 19:15:21 -0400

    Code cleanup and refinements.

--- src/main/java/health/ere/ps/service/common/util/BundleJsonInfoExtractor.java
+++ src/main/java/health/ere/ps/service/common/util/BundleJsonInfoExtractor.java
@@ -5,15 +5,12 @@ import org.jboss.logging.Logger;
 
 import java.io.StringReader;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-
-import health.ere.ps.service.logging.EreLogger;
 
 /**
  * A Utility class used to extract information of particular importance from a bundle json string.
