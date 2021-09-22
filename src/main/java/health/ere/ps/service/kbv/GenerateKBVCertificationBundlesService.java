d8c87afa3ae8ec8fffdac8579046dcbf753c856d
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2021-09-22 12:39:11 +0200

    Created all test data

--- src/main/java/health/ere/ps/service/kbv/GenerateKBVCertificationBundlesService.java
+++ src/main/java/health/ere/ps/service/kbv/GenerateKBVCertificationBundlesService.java
@@ -858,7 +858,7 @@ public class GenerateKBVCertificationBundlesService {
                 .setType("Device")
                 .getIdentifier()
                 .setSystem("https://fhir.kbv.de/NamingSystem/KBV_NS_FOR_Pruefnummer")
-                .setValue("Y/400/1904/36/112");
+                .setValue("Y/400/1904/36/344");
 
         composition.setTitle("elektronische Arzneimittelverordnung");
 
