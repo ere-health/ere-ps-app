b0d623b36d927851e19add1063caebd25c92b652
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2021-11-04 17:34:14 +0100

    Added more test cases for generating E-Prescriptions

--- src/test/java/health/ere/ps/service/gematik/MassGenerator2Test.java
+++ src/test/java/health/ere/ps/service/gematik/MassGenerator2Test.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,9 +27,11 @@ import java.util.logging.Logger;
 import javax.inject.Inject;
 
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Coverage;
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.MedicationRequest;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Reference;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -103,11 +104,23 @@ public class MassGenerator2Test {
 
     @Test
     void testCreateERezeptMassCreate() throws Exception {
+        createERezeptMassCreate("../secret-test-print-samples/CIDA/egk/cards.txt");
+    }
+
+    @Test
+    void testCreateERezeptMassCreateManuel() throws Exception {
+        createERezeptMassCreate("src/test/resources/manuels-egk/egk.txt", "src/test/resources/manuels-egk/insurance.txt");
+    }
+
+    void createERezeptMassCreate(String cardsString) throws Exception {
+        createERezeptMassCreate(cardsString, null);
+    }
+    void createERezeptMassCreate(String cardsString, String insuranceString) throws Exception {
         int i = 0;
         DocumentService documentService = new DocumentService();
                 documentService.init();
         
-        List<String> cards = Files.readAllLines(Paths.get("../secret-test-print-samples/CIDA/egk/cards.txt"));
+        List<String> cards = Files.readAllLines(Paths.get(cardsString));
 
 
         eRezeptWorkflowService.activateComfortSignature();
@@ -115,89 +128,106 @@ public class MassGenerator2Test {
                                 .withZone(ZoneOffset.UTC)
                                 .format(Instant.now());
         FileWriter fw = new FileWriter("target/e-rezept-report-"+thisMomentString+".csv");
-        fw.write("Id,Filename,AccessCode,Patient\n");
-        for(String card : cards) {
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("../secret-test-print-samples/CIDA/templates/"), "*")) {
+        fw.write("Id,Filename,AccessCode,Patient,Insurance\n");
 
-                for (Path entry : stream) {
-                    File file = entry.toFile();
-                    List<File> bundleFile;
-                    if(!file.isDirectory()) {
-                        bundleFile = Arrays.asList(file);
-                    } else {
-                        bundleFile = Arrays.asList(file.listFiles());
-                    }
-                    List<Bundle> bundles = new ArrayList<>();
-                    for(File myFile : bundleFile) {
-                        InputStream fileStream = new FileInputStream(myFile);
-                        String template = new String(fileStream.readAllBytes());
-                        fileStream.close();
-                        Bundle bundle = iParser.parseResource(Bundle.class, template);
-                        bundle.setId(UUID.randomUUID().toString());
-                        try {
-                            ((MedicationRequest)bundle.getEntry().stream().filter(e -> e.getResource() instanceof MedicationRequest).findAny().get().getResource()).setAuthoredOnElement(new DateTimeType(new Date(), TemporalPrecisionEnum.DAY));
-                        } catch(NoSuchElementException ex) {
-                            ex.printStackTrace();
-                        }
-                        try {
-                            Patient patient = ((Patient)bundle.getEntry().stream().filter(e -> e.getResource() instanceof Patient).findAny().get().getResource());
-                            patient.getIdentifier().get(0).setValue(card);
-                        } catch(Exception ex) {
-                            ex.printStackTrace();
-                        }
-                        ValidationResult validationResult = prescriptionBundleValidator.validateResource(bundle, true);
-                        if(validationResult.isSuccessful()) {
-                            bundles.add(bundle);
+        List<String> insuranceList = Arrays.asList("Default");
+        if(insuranceList != null) {
+            insuranceList = Files.readAllLines(Paths.get(insuranceString));
+        }
+        for(String singleInsurance : insuranceList) {
+            for(String card : cards) {
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("../secret-test-print-samples/CIDA/templates/"), "*")) {
+                    for (Path entry : stream) {
+                        File file = entry.toFile();
+                        List<File> bundleFile;
+                        if(!file.isDirectory()) {
+                            bundleFile = Arrays.asList(file);
                         } else {
-                            log.info(entry.toFile().getName()+" is not valid");
+                            bundleFile = Arrays.asList(file.listFiles());
                         }
-                    }
-                    // System.out.println(iParser.encodeResourceToString(bundle));
-                    //if(2 == 1+1) {
-                    //    break;
-                    //}
-                
-                    List<BundleWithAccessCodeOrThrowable> bundleWithAccessCodeOrThrowables = eRezeptWorkflowService.createMultipleERezeptsOnPrescriptionServer(bundles);
-                    String thisMoment = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH_mm_ssX")
-                            .withZone(ZoneOffset.UTC)
-                            .format(Instant.now());
-                            
-                    if(bundleWithAccessCodeOrThrowables.size() > 0 && bundleWithAccessCodeOrThrowables.get(0).getThrowable() != null) {
-                        StringWriter sw = new StringWriter();
-                        PrintWriter pw = new PrintWriter(sw);
-                        bundleWithAccessCodeOrThrowables.get(0).getThrowable().printStackTrace(pw);
-                        Files.write(Paths.get("target/"+entry.toFile().getName().replace(".xml", "")+"-"+card+"-"+ thisMoment + ".txt"), sw.toString().getBytes());
-                        if(bundleWithAccessCodeOrThrowables.get(0).getBundle() != null) {
-                            Files.write(Paths.get("target/"+entry.toFile().getName().replace(".xml", "")+"-"+card+"-"+ thisMoment + ".xml"), iParser.encodeResourceToString(bundleWithAccessCodeOrThrowables.get(0).getBundle()).getBytes());
+                        List<Bundle> bundles = new ArrayList<>();
+                        for(File myFile : bundleFile) {
+                            InputStream fileStream = new FileInputStream(myFile);
+                            String template = new String(fileStream.readAllBytes());
+                            fileStream.close();
+                            Bundle bundle = iParser.parseResource(Bundle.class, template);
+                            bundle.setId(UUID.randomUUID().toString());
+                            try {
+                                ((MedicationRequest)bundle.getEntry().stream().filter(e -> e.getResource() instanceof MedicationRequest).findAny().get().getResource()).setAuthoredOnElement(new DateTimeType(new Date(), TemporalPrecisionEnum.DAY));
+                            } catch(NoSuchElementException ex) {
+                                ex.printStackTrace();
+                            }
+                            try {
+                                Patient patient = ((Patient)bundle.getEntry().stream().filter(e -> e.getResource() instanceof Patient).findAny().get().getResource());
+                                patient.getIdentifier().get(0).setValue(card);
+                            } catch(Exception ex) {
+                                ex.printStackTrace();
+                            }
+                            if(!singleInsurance.equals("Default")) {
+                                Coverage coverage = ((Coverage)bundle.getEntry().stream().filter(e -> e.getResource() instanceof Coverage).findAny().get().getResource());
+                                ((Reference)coverage.getPayor().get(0)).getIdentifier().setValue(singleInsurance);
+                            }
+                            ValidationResult validationResult = prescriptionBundleValidator.validateResource(bundle, true);
+                            if(validationResult.isSuccessful()) {
+                                bundles.add(bundle);
+                            } else {
+                                log.info(entry.toFile().getName()+" is not valid");
+                            }
                         }
-                        fw.flush();
-                    } else if(bundleWithAccessCodeOrThrowables.size() > 0) {
-                        ByteArrayOutputStream a = documentService.generateERezeptPdf(bundleWithAccessCodeOrThrowables);
-                        String fileName = entry.toFile().getName().replace(".xml", "")+"-"+card+"-" + thisMoment + ".pdf";
-                        Files.write(Paths.get("target/"+fileName), a.toByteArray());
-                        for(BundleWithAccessCodeOrThrowable bundleWithAccessCodeOrThrowable : bundleWithAccessCodeOrThrowables) {
-                            fw.write(bundleWithAccessCodeOrThrowable.getBundle().getIdentifier().getValue()+","+fileName+","+bundleWithAccessCodeOrThrowable.getAccessCode()+","+card+"\n");
+                        // System.out.println(iParser.encodeResourceToString(bundle));
+                        //if(2 == 1+1) {
+                        //    if(bundles.size() == 0) {
+                        //        continue;
+                        //    }
+                        //    Files.write(Paths.get("target/"+entry.toFile().getName().replace(".xml", "")+"-Single-Test.xml"), iParser.encodeResourceToString(bundles.get(0)).getBytes());
+                        //    if(true) {
+                        //        break;
+                        //    }
+                        //
+                        //}
+                    
+                        List<BundleWithAccessCodeOrThrowable> bundleWithAccessCodeOrThrowables = eRezeptWorkflowService.createMultipleERezeptsOnPrescriptionServer(bundles);
+                        String thisMoment = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH_mm_ssX")
+                                .withZone(ZoneOffset.UTC)
+                                .format(Instant.now());
+                                
+                        if(bundleWithAccessCodeOrThrowables.size() > 0 && bundleWithAccessCodeOrThrowables.get(0).getThrowable() != null) {
+                            StringWriter sw = new StringWriter();
+                            PrintWriter pw = new PrintWriter(sw);
+                            bundleWithAccessCodeOrThrowables.get(0).getThrowable().printStackTrace(pw);
+                            Files.write(Paths.get("target/"+entry.toFile().getName().replace(".xml", "")+"-"+card+"-"+ thisMoment + ".txt"), sw.toString().getBytes());
+                            if(bundleWithAccessCodeOrThrowables.get(0).getBundle() != null) {
+                                Files.write(Paths.get("target/"+entry.toFile().getName().replace(".xml", "")+"-"+card+"-"+ thisMoment + ".xml"), iParser.encodeResourceToString(bundleWithAccessCodeOrThrowables.get(0).getBundle()).getBytes());
+                            }
+                            fw.flush();
+                        } else if(bundleWithAccessCodeOrThrowables.size() > 0) {
+                            ByteArrayOutputStream a = documentService.generateERezeptPdf(bundleWithAccessCodeOrThrowables);
+                            String fileName = entry.toFile().getName().replace(".xml", "")+"-"+card+"-" + thisMoment + ".pdf";
+                            Files.write(Paths.get("target/"+fileName), a.toByteArray());
+                            for(BundleWithAccessCodeOrThrowable bundleWithAccessCodeOrThrowable : bundleWithAccessCodeOrThrowables) {
+                                fw.write(bundleWithAccessCodeOrThrowable.getBundle().getIdentifier().getValue()+","+fileName+","+bundleWithAccessCodeOrThrowable.getAccessCode()+","+card+","+singleInsurance+"\n");
+                            }
+                            fw.flush();
+                        } else {
+                            log.warning(file.getName()+" produced no bundleWithAccessCodeOrThrowables");
                         }
-                        fw.flush();
-                    } else {
-                        log.warning(file.getName()+" produced no bundleWithAccessCodeOrThrowables");
-                    }
 
-                    log.info("Time: "+thisMoment);
-                    
-                    i++;
+                        log.info("Time: "+thisMoment);
+                        
+                        i++;
+                    }
+                    if(i == 100) {
+                        eRezeptWorkflowService.deactivateComfortSignature();
+                        eRezeptWorkflowService.activateComfortSignature();
+                    }
+                } catch (Exception ex) {
+                    // I/O error encounted during the iteration, the cause is an IOException
+                    log.info("Exception: "+ex);
+                        
+                    ex.printStackTrace();
                 }
-                if(i == 100) {
-                    eRezeptWorkflowService.deactivateComfortSignature();
-                    eRezeptWorkflowService.activateComfortSignature();
-                }
-            } catch (DirectoryIteratorException ex) {
-                // I/O error encounted during the iteration, the cause is an IOException
-                log.info("Exception: "+ex);
-                    
-                ex.printStackTrace();
+                // break;
             }
-            // break;
         }
         eRezeptWorkflowService.deactivateComfortSignature();
         fw.close();
