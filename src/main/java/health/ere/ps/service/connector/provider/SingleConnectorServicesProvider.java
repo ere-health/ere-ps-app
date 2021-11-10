974fc3a4971b3b53587812b52d4c089ef5da25f3
ruszkipista <oi5672@gmail.com>
2021-11-10 10:18:21 +0000

    add first test (not working)

--- src/main/java/health/ere/ps/service/connector/provider/SingleConnectorServicesProvider.java
+++ src/main/java/health/ere/ps/service/connector/provider/SingleConnectorServicesProvider.java
@@ -1,14 +1,19 @@
 package health.ere.ps.service.connector.provider;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.Base64;
 import java.util.logging.Logger;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.enterprise.event.Event;
 
@@ -29,7 +34,7 @@ public class SingleConnectorServicesProvider extends AbstractConnectorServicesPr
             String clientCertificateString = userConfig.getConfigurations().getClientCertificate().substring(33);
             byte[] clientCertificateBytes = Base64.getDecoder().decode(clientCertificateString);
             
-            // byte[] clientCertificateBytes = getCertificateFromUrlString(SuserConfig.getConfigurations().getClientCertificate(), userConfig.getConfigurations().getClientCertificatePassword());
+            // byte[] clientCertificateBytes = getCertificateFromUriString(userConfig.getConfigurations().getClientCertificate(), userConfig.getConfigurations().getClientCertificatePassword());
             try (ByteArrayInputStream certificateInputStream = new ByteArrayInputStream(clientCertificateBytes)) {
                 this.secretsManagerService.setUpSSLContext(userConfig.getConfigurations().getClientCertificatePassword(), certificateInputStream);
             } catch (NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException
@@ -44,8 +49,36 @@ public class SingleConnectorServicesProvider extends AbstractConnectorServicesPr
         initializeServices();
     }
 
-    public static byte[] getCertificateFromUrlString(String url, String password) {
+    public static byte[] getCertificateFromUriString(String uri, String keystorePassword) throws URISyntaxException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        String keystoreBase64;
+        KeyStore store = KeyStore.getInstance("PKCS12");
+
         // parse URL with java.net.URL
+        URI uriParser = new URI(uri);
+        String scheme = uriParser.getScheme();
+        if (scheme.equalsIgnoreCase("data")){
+            String[] schemeSpecificParts = uriParser.getSchemeSpecificPart().split(";");
+            String contentType = schemeSpecificParts[0];
+            if (contentType.equalsIgnoreCase("application/x-pkcs12")){
+                String[] dataParts = schemeSpecificParts[1].split(",");
+                String encodingType = dataParts[0];
+                if (encodingType.equalsIgnoreCase("base64")){
+                    keystoreBase64 = dataParts[1];
+                    try (ByteArrayInputStream keystoreInputStream = new ByteArrayInputStream(Base64.getDecoder().decode(keystoreBase64))){
+                      store.load(keystoreInputStream, keystorePassword.toCharArray());
+                    }
+                    Certificate certificate = store.getCertificate(store.aliases().nextElement());
+                    return certificate.getEncoded();
+                }
+            }
+        } else if (scheme.equalsIgnoreCase("file")) {
+            String[] schemeSpecificParts = uriParser.getSchemeSpecificPart().split("?");
+            String keystoreFile = schemeSpecificParts[0];
+            String keyAlias = schemeSpecificParts[1];
+            store.load(new FileInputStream(keystoreFile), keystorePassword.toCharArray());
+            Certificate certificate = store.getCertificate(keyAlias);
+            return certificate.getEncoded();
+        }
 
         // figure out if it is a data or a file url
 
