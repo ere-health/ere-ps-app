6b07712f2278b9d931cf2a64d06421b58d65a8b9
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2022-02-05 19:59:05 +0000

    Fixed issue with installer

--- linux-service/installer.sh
+++ linux-service/installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -r ../target/quarkus-app /opt/ere-health
+mkdir -p /opt/ere-health/application/quarkus-app/
+mv /opt/ere-health/server.keystore /opt/ere-health/application/quarkus-app/server.keystore
 cp run.sh /opt/ere-health
 cp ere-health.service /etc/systemd/system/
 sudo systemctl daemon-reload
