dd80b022ed8740a3bea48d3ee26a54edcafeffaf
Sara Rodrigues <saravdp@live.com.pt>
2022-10-08 22:35:11 +0200

    Test Functions

--- src/test/java/health/ere/ps/resource/config/UserConfigurationsResourceTest.java
+++ src/test/java/health/ere/ps/resource/config/UserConfigurationsResourceTest.java
@@ -1,40 +1,65 @@
 package health.ere.ps.resource.config;
 
-import static org.mockito.Mockito.when;
-
-import java.util.UUID;
-
-import javax.enterprise.context.ApplicationScoped;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import health.ere.ps.model.config.UserConfigurations;
-import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 public class UserConfigurationsResourceTest {
-    
-    @Test
-    public void testGetConfigEndpoint() {
-        given()
-          .when().get("http://localhost:8080/config")
-          .then()
-             .statusCode(200);
+  //Test GET request status code
+  @Test
+  public void testGetConfig() {
+    given()
+        .header("Content-Type", "application/json")
+        .when()
+        .get("http://localhost:8080/config")
+        .then()
+        .statusCode(200)
+        .log()
+        .all();
+  }
 
-             
-    }
-    @Test
-    public void testPutConfigNullEndpoint() {
-        UserConfigurations userConfiguration = new UserConfigurations();
-        given()
-          .when().put("http://localhost:8080/config")
-          .then()
-             .statusCode(415);
-            //  .body(is("hello"));
-    }
+  // Testing HTTP Error Codes
+  @Test
+  public void testGetConfigErrorNotFound() {
+    RestAssured.baseURI = "http://localhost:8080/config";
+    RequestSpecification httpRequest = RestAssured.given();
+    Response response = httpRequest.request(Method.GET, "/1");
+    int statusCode = response.getStatusCode();
+    assertEquals(statusCode, 404, "Correct status code is not returned");
+  }
+
+  @Test
+  public void testPutConfigNoHeaderError() {
+
+    RestAssured.baseURI = "http://localhost:8080/config";
+    RequestSpecification httpRequest = RestAssured.given();
+    Response response = httpRequest.request(Method.PUT, "/");
+    int statusCode = response.getStatusCode();
+    assertEquals(statusCode, 415, "Correct status code is not returned");
+
+  }
+
+  @Test
+  public void testPutConfigIsNull() {       
+    given()
+        .when()
+        .header("X-erp-user","l")
+        .header("Content-Type", "application/json")
+        .header("Host", "erp.zentral.erp.splitdns.ti-dienste.de")
+        .header("Authorization", "Bearer eyJraWQ.ewogImL2pA10Qql22ddtutrvx4FsDlz.rHQjEmB1lLmpqn9J")
+        .header("User-Agent", "E-Rezept FdV 1.0.0")
+        .header("Accept", "application/fhir+json;charset=utf-8")
+        .put("http://localhost:8080/config")
+        .then()
+        .statusCode(500)
+        .log()
+        .all();
+  }
 }
