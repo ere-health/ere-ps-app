7847292cbf6cdba67c56739c4ae71e7723e9fa5f
Pablo Escaida <pescaida@gmail.com>
2022-11-01 15:21:12 +0100

    UriUtilsTest.java ist fertig geschrieben und laeuft gut

---
+++ src/test/java/health/ere/ps/service/idp/client/authentication/UriUtilsTest.java
@@ -0,0 +1,27 @@
+package health.ere.ps.service.idp.client.authentication;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import health.ere.ps.exception.idp.IdpException;
+
+public class UriUtilsTest {
+  @Test
+  public void myTest() throws IdpException  {
+      final String testUriString = "https://www.google.com?hello=Pablo&bye=2";
+
+      final Map<String, String> getValues = UriUtils.extractParameterMap(testUriString) ;
+
+      assertEquals(2, getValues.size(), "The URL should parse 2 parameters");
+      
+      assertEquals("Pablo", getValues.get("hello"), 
+        "System should correctly parse the first parameter");
+
+
+      assertTrue(true);
+  }
+}
