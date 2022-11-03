d5cbc1b2487eeaf3e58c83c7415e2285bc4610c4
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2022-11-03 10:50:39 +0100

    ERE-606 added application.properties outside of jar

--- linux-service/installer.sh
+++ linux-service/installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -r ../target/quarkus-app /opt/ere-health
+mkdir /opt/ere-health/config
+cp ../src/main/resources/application.properties /opt/ere-health/config
 cp run.sh /opt/ere-health
 cp ere-health.service /etc/systemd/system/
 sudo systemctl daemon-reload
