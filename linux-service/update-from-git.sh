14f4b99679dd1a7e3dc6cb6687a0c750e231c0be
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2024-04-14 21:31:32 +0200

    ERE-634 fixed quarkus-app folder

--- linux-service/update-from-git.sh
+++ linux-service/update-from-git.sh
@@ -3,7 +3,7 @@ git pull
 mvn clean
 mvn -DskipTests=true install
 sudo systemctl stop ere-health
-sudo cp target/quarkus-app/app/* /opt/ere-health/quarkus-app/app
+sudo cp target/quarkus-app/app/* /opt/ere-health/app
 sleep 5
 sudo systemctl start ere-health
 sudo systemctl status ere-health
