b0e0dc367615fa19070623d390765485e42d3baf
Clemens Landsberg <clemens.landsberg@scoop-software.de>
2024-06-11 16:45:23 +0200

    NFCKT-808 Add prometheus agent. Keystore/pw is for the local test setup

--- linux-service/installer.sh
+++ linux-service/installer.sh
@@ -5,6 +5,9 @@ mkdir /opt/ere-health/config
 cp ../src/main/resources/application.properties /opt/ere-health/config
 cp run.sh /opt/ere-health/
 cp ere-health.service /etc/systemd/system/
+mvn -U dependency:copy -Dartifact=io.prometheus.jmx:jmx_prometheus_javaagent:1.0.1:jar -DoutputDirectory=/opt/ere-health
+cp src/main/resources/prometheus.yaml /opt/ere-health
+cp -r prometheus/ /opt/ere-health/
 sudo systemctl daemon-reload
 sudo systemctl enable ere-health.service
 sudo systemctl start ere-health
--- linux-service/run.sh
+++ linux-service/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /usr/bin/java -javaagent:jmx_prometheus_javaagent-1.0.1.jar=20000:prometheus.yaml -Dquarkus.profile=PU -jar quarkus-run.jar
+sudo /usr/bin/java -javaagent:jmx_prometheus_javaagent-1.0.1.jar=20001:prometheus.yaml -Dquarkus.profile=PU -jar quarkus-run.jar
