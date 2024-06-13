eb1f8059a989f63be71b81eb67307a10c5fa8716
Clemens Landsberg <clemens.landsberg@scoop-software.de>
2024-06-13 16:07:13 +0200

    NFCKT-814 correct update script; removed unneeded interface methodes

--- linux-service/update-from-git.sh
+++ linux-service/update-from-git.sh
@@ -6,9 +6,9 @@ sudo systemctl stop ere-health
 sudo rm -r /opt/ere-health/app/*
 sudo cp target/quarkus-app/app/* /opt/ere-health/app
 sudo rm -r /opt/ere-health/quarkus/*
-sudo cp target/quarkus-app/quarkus/* /opt/ere-health/quarkus
+sudo cp -r target/quarkus-app/quarkus/* /opt/ere-health/quarkus
 sudo rm -r /opt/ere-health/lib/*
-sudo cp target/quarkus-app/lib/* /opt/ere-health/lib
+sudo cp -r target/quarkus-app/lib/* /opt/ere-health/lib
 sleep 5
 sudo systemctl start ere-health
 sudo systemctl status ere-health
