dc8f20d75ea6052d5b496eb8176ddfa925c0c54a
alexanderremmes <a.remmes@icloud.com>
2024-07-18 10:53:15 +0200

    NFCKT-934 WIP

---
+++ src/main/java/health/ere/ps/service/gematik/FailedRejectEntry.java
@@ -0,0 +1,39 @@
+package health.ere.ps.service.gematik;
+
+import health.ere.ps.config.RuntimeConfig;
+
+public class FailedRejectEntry {
+    private String prescriptionId;
+    private String secret;
+    private RuntimeConfig runtimeConfig;
+
+    public FailedRejectEntry(String prescriptionId, String secret, RuntimeConfig runtimeConfig) {
+        this.prescriptionId = prescriptionId;
+        this.secret = secret;
+        this.runtimeConfig = runtimeConfig;
+    }
+
+    public String getPrescriptionId() {
+        return prescriptionId;
+    }
+
+    public void setPrescriptionId(String prescriptionId) {
+        this.prescriptionId = prescriptionId;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public RuntimeConfig getRuntimeConfig() {
+        return runtimeConfig;
+    }
+
+    public void setRuntimeConfig(RuntimeConfig runtimeConfig) {
+        this.runtimeConfig = runtimeConfig;
+    }
+}
