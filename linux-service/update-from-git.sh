59993542d697b7e4dd5f53049def56bdbd499d11
Theo Diefenthal <theo.diefenthal@scoop-software.de>
2024-07-19 18:05:15 +0200

    When using update-from-git.sh, log out deployed git tag

--- linux-service/update-from-git.sh
+++ linux-service/update-from-git.sh
@@ -9,6 +9,7 @@ sudo rm -r /opt/ere-health/quarkus/*
 sudo cp -r target/quarkus-app/quarkus/* /opt/ere-health/quarkus
 sudo rm -r /opt/ere-health/lib/*
 sudo cp -r target/quarkus-app/lib/* /opt/ere-health/lib
+sudo bash -c 'echo "$(date --iso-8601=s) $(git rev-parse --verify HEAD)" >> /opt/ere-health/update-linux-deployments.log'
 sleep 5
 sudo systemctl start ere-health
 sudo systemctl status ere-health
