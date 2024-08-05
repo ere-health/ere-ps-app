0d255505f409f064395b77aee59b61853bb7918a
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2024-08-05 15:54:25 +0200

    NFCKT-1036 release 1.0.0-4

--- distributions/clean.sh
+++ distributions/clean.sh
@@ -7,11 +7,11 @@ DEB_VERSION="1.0.0"
 
 rm -R ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}
 rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}.orig.tar.gz
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-2_amd64.build
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-2_amd64.buildinfo
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-2_amd64.changes
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-2.debian.tar.xz
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-2.dsc
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-2_all.deb
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-4_amd64.build
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-4_amd64.buildinfo
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-4_amd64.changes
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-4.debian.tar.xz
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-4.dsc
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-4_all.deb
 rm -R deb-package/debian/ere-health
 rm -R deb-package/debian/.debhelper
