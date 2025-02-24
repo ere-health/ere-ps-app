8934f89dbdf51707fedd4a77e43552cf9ac92126
Manuel Blechschmidt <manuel.blechschmidt@service-health.de>
2025-02-24 17:10:18 +0100

    NFCKT-1132 released ere-ps-app 1.0.0-13

--- distributions/clean.sh
+++ distributions/clean.sh
@@ -7,11 +7,11 @@ DEB_VERSION="1.0.0"
 
 rm -R ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}
 rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}.orig.tar.gz
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-12_amd64.build
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-12_amd64.buildinfo
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-12_amd64.changes
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-12.debian.tar.xz
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-12.dsc
-rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-12_all.deb
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-13_amd64.build
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-13_amd64.buildinfo
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-13_amd64.changes
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-13.debian.tar.xz
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-13.dsc
+rm ${DPKG_BUILD_PACKAGE}_${DEB_VERSION}-13_all.deb
 rm -R deb-package/debian/ere-health
 rm -R deb-package/debian/.debhelper
